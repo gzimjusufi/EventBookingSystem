364454880b1eb1008c0deeea4a1a9c2b738f84a9
Gzim Jusufi <jusufigzim2@gmail.com>
2026-06-11 22:23:36 +0200

    fixed eventcontroller might have been the problem

--- EventBookingApi/Controllers/EventController.cs
+++ EventBookingApi/Controllers/EventController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace EventBookingApi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class EventController : ControllerBase
 {
     private readonly IEventService _eventService;
